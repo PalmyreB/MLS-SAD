7e8c0f756790a2005fa8d619344d57efd612566e
unknown <Palmyre@elevate.encs.concordia.ca>
2019-06-26 13:57:31 -0400

    Add a case to example of "Not Caching Objects' Elements"

--- MLS SAD Tests/src/mlssad/codesmells/test/TestCaseNotCachingObjectsElements.java
+++ MLS SAD Tests/src/mlssad/codesmells/test/TestCaseNotCachingObjectsElements.java
@@ -11,8 +11,11 @@ public class TestCaseNotCachingObjectsElements extends AbstractCodeSmellTestCase
 		super.setUp();
 		aPathC = "../MLS SAD Tests/rsc/CodeSmellsC/src/codeSmellsC/NotCachingObjectsElements.c";
 		aPathJava = "../MLS SAD Tests/rsc/CodeSmellsJNI/src/codeSmellsJava/NotCachingObjectsElements.java";
-		expectedSmells = new HashSet<>(Arrays.asList(new MLSCodeSmell("NotCachingObjectsElements", "\"group\"",
-				"Java_codeSmellsJava_NotCachingObjectsElements_00024User_checkGroup", "", "", aPathC)));
+		expectedSmells = new HashSet<>(Arrays.asList(
+				new MLSCodeSmell("NotCachingObjectsElements", "\"group\"",
+						"Java_codeSmellsJava_NotCachingObjectsElements_00024User_setGroup", "", "", aPathC),
+				new MLSCodeSmell("NotCachingObjectsElements", "\"group\"",
+						"Java_codeSmellsJava_NotCachingObjectsElements_00024User_checkGroup", "", "", aPathC)));
 		detector = new NotCachingObjectsElementsDetection();
 	}
 
