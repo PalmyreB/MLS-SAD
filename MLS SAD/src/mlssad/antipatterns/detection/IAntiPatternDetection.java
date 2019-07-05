4d715c022eee07a06f449bf8590571cc601ebb7e
unknown <Palmyre@elevate.encs.concordia.ca>
2019-07-05 12:54:00 -0400

    Clean `IAntiPatternDetection`

--- MLS SAD/src/mlssad/antipatterns/detection/IAntiPatternDetection.java
+++ MLS SAD/src/mlssad/antipatterns/detection/IAntiPatternDetection.java
@@ -26,21 +26,18 @@ import mlssad.kernel.impl.MLSAntiPattern;
 
 public interface IAntiPatternDetection {
 
-	//	final String FILE_QUERY = "//unit";
 	final String C_FILES_QUERY = "//unit[@language='C++' or @language='C']";
-
 	final String JAVA_FILES_QUERY = "//unit[@language='Java']";
-
 	final String CLASS_QUERY = "class/name";
-
 	final String PACKAGE_QUERY = "package/name"; // Call on unit
 	final String FILEPATH_QUERY = "@filename"; // Call on unit
 	final String NATIVE_QUERY =
 		"descendant::function_decl[specifier='native']/name";
-	//	String PACKAGE_QUERY = "preceding-sibling::package/name";
+
 	void detect(final Document xml);
+
 	String getAntiPatternName();
-	//		String getRuleCardFile();
+
 	// TODO: We may have a vocabulary problem:
 	// Do we detect antipatterns or rather micro-architectures
 	// similar to some anti-patterns? Similarly to what happens
@@ -52,5 +49,4 @@ public interface IAntiPatternDetection {
 	void output(final PrintWriter aWriter);
 
 	void output(final PrintWriter aWriter, int count);
-
 }
